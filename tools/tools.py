741e06b844aa53b829bc964f3177bf65847d229a
Hahnnz <lphion96@naver.com>
2018-03-04 17:55:13 +0900

    add float num

--- tools/tools.py
+++ tools/tools.py
@@ -8,6 +8,6 @@ def markJoints(img, joints):
   for i in range(len(joints)):
   	x,y = joints[i]
   	cv2.circle(img, (x,y), 4, (255,0, 0), thickness=-1)
-  	cv2.putText(img, str(i+1), (x,y), font, (100,100,100), 1, cv2.LINE_AA)
+  	cv2.putText(img, str(i+1), (x,y), font, 0.5, (100,100,100), 1, cv2.LINE_AA)
 
   return img 
