1cb3cfec47f1bc9dd48dc452a90e70f199a33a12
Hahnnz <lphion96@naver.com>
2018-03-04 16:01:08 +0900

    add some def

---
+++ tools/tools.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def markJoints(img, joints):
+  circleSize=10
+  font=cv2.FONT_HERSHEY_SIMPLEX
+
+  for i in range(len(joints)):
+  	x,y = joints[i]
+  	cv2.circle(img, (x,y), 4, (255,0, 0), thickness=-1)
+  	cv2.putText(img, str(i+1), (x,y), font, (100,100,100), 1, cv2.LINE_AA)
+
+  return img 
