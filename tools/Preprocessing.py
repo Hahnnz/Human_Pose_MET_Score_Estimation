f79625f232f306b1c716229bec695e15c7b8d6ea
Hahnnz <lphion96@naver.com>
2018-03-04 17:48:46 +0900

    modified

--- tools/Preprocessing.py
+++ tools/Preprocessing.py
@@ -25,23 +25,18 @@ class prepare_dataset:
 
     return "Arverage X : "+avg_x+"\nArverage Y : "+avg_y
 
-  # if coos is "[00, 00]",
-  def convert_str_to_int(dataset):
-    coors=np.zeros([len(dataset),14,2],dtype=int)
+  def prepare(dataset):
+    coor_set=np.zeros([len(dataset),14,2],dtype=int)
+    img_set = np.zeros([len(dataset),256,256,3])
     for i in range(len(dataset)):
       for j in range(14):
         a=0
         while True:
           if dataset[i][j+1][a]==",": break
           else : a+=1
-          coors[i][j]=int(dataset[i][j+1][1:a]),int(dataset[i][j+1][a:1:-1]) 
-    return coors
-  
-  def prepare(dataset):
-  	img_set = np.zeros([len(dataset),256,256,3])
-  	coor_set = convert_str_to_int(dataset)
+        coor_set[i][j]=int(dataset[i][j+1][1:a]),int(dataset[i][j+1][a+1:-1])
 
-  	for i in range(len(dataset)):
+    for i in range(len(dataset)):
   	  img=cv2.imread(dataset[i][0])
 
   	  img_set[i]=cv2.resize(img,(256,256))
@@ -51,4 +46,5 @@ class prepare_dataset:
   	    else : coor_set[i][j][0]=coor_set[i][j][0]*(256/img.shape[1])
   	    if coor_set[i][j][1]==-1 : coor_set[i][j][1]=-1
   	    else : coor_set[i][j][1]=coor_set[i][j][1]*(256/img.shape[0])
-  	return img_set, coor_set
+  	
+    return img_set,coor_set
