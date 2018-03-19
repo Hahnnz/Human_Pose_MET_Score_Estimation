c653a5cbda88d87d3f79e6a89cd1d75e2b3918d2
Hahnnz <lphion96@naver.com>
2018-03-20 01:15:22 +0900

    change cv2 to skimage marker

--- tools/tools.py
+++ tools/tools.py
@@ -1,16 +1,19 @@
 import cv2
 import os
+import skimage
+from skimage import draw
 
-def markJoints(img, joints):
-  circleSize=10
-  font=cv2.FONT_HERSHEY_SIMPLEX
-
-  for i in range(len(joints)):
-  	x,y = joints[i]
-  	cv2.circle(img, (x,y), 4, (255,0, 0), thickness=-1)
-  	cv2.putText(img, str(i+1), (x,y), font, 0.5, (100,100,100), 1, cv2.LINE_AA)
-
-  return img 
+def markJoints(img, joints):  
+    circSize=5
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for i in range(14):
+        x = int(joints[i,0])
+        y = int(joints[i,1])
+        if x!=-1:
+            rr, cc = skimage.draw.circle(y, x, circSize)
+            skimage.draw.set_color(img, (rr, cc), (1,0,0))
+            cv2.putText(img, str(i+1), (x,y), font, 0.5, (0.5,0.5,0.5), 2, cv2.LINE_AA)
+    return img
 
 def set_GPU(device_num):
   os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
