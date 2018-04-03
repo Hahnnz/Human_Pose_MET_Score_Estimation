ee32245df8c79ee9b7484295812985150605e9a2
Hahnnz <lphion96@naver.com>
2018-04-03 16:34:12 +0900

    modified

--- tools/dataset.py
+++ tools/dataset.py
@@ -18,14 +18,14 @@ def create(DATASET_ROOT="./dataset/", reshape_size=[400,400],active_joint_absenc
         joints_set = joints["joints"].transpose(2,1,0)[:, :, :2]
     
     i=0
-    img_set = np.zeros([len(joints),reshape_size[0],reshape_size[1],3])
-    for img_path in sorted(glob.glob(os.path.join("./images/", '*.jpg'))):
+    img_set = np.zeros([len(joints_set),reshape_size[0],reshape_size[1],3])
+    for img_path in sorted(glob.glob(os.path.join(DATASET_ROOT+"images/", '*.jpg'))):
         img = skimage.io.imread(img_path)
         img_set[i]=skimage.transform.resize(img,reshape_size)
         for j in range(14):
-                if joints[i][j][0]==-1: joints[i][j][0]=-1
-                else: joints[i][j][0]=joints[i][j][0]*(reshape_size[0]/img.shape[1])
-                if joints[i][j][1]==-1: joints[i][j][1]=-1
-                else: joints[i][j][1]=joints[i][j][1]*(reshape_size[1]/img.shape[0])
+                if active_joint_absence and joints_set[i][j][0]==-1: joints_set[i][j][0]=-1
+                else: joints_set[i][j][0]=joints_set[i][j][0]*(reshape_size[0]/img.shape[1])
+                if active_joint_absence and joints_set[i][j][1]==-1: joints_set[i][j][1]=-1
+                else: joints_set[i][j][1]=joints_set[i][j][1]*(reshape_size[1]/img.shape[0])
         i+=1
     return {'images':img_set, 'joints':joints_set}
