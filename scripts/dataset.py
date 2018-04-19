c9f4834e1429b0713c82408aef9cdc7d6acfd014
Hahnnz <lphion96@naver.com>
2018-04-19 14:31:38 +0900

    modified

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,37 +1,41 @@
 from scipy.io import loadmat
 import numpy as np
-import skimage
+import skimage, glob, os, copy
 from skimage import io, transform
-import glob, os
 
 # For lsp dataset...
-def create(DATASET_ROOT="./dataset/", reshape_size=[400,400],active_joint_absence=False, divide_train_test=True):
-    joints = loadmat(DATASET_ROOT+"joints.mat")
+class lsp:
+    def __init__(self):
+        self.trainset, self.testset = __create(DATASET_ROOT="./dataset/", reshape_size=[227,227])
     
-    if active_joint_absence :
-        joints = joints["joints"].transpose(2,1,0)[:, :, :]
-        for i in range(joints.shape[0]):
-            for j in range(joints.shape[1]):
-                if joints[i][j][2] == 1: joints[i][j][:1]=[-1, -1]
-        joints_set = joints[:,:,:2]
-    else :
-        joints_set = joints["joints"].transpose(2,1,0)[:, :, :2]
-    
-    i=0
-    img_set = np.zeros([len(joints_set),reshape_size[0],reshape_size[1],3])
-    for img_path in sorted(glob.glob(os.path.join(DATASET_ROOT+"images/", '*.jpg'))):
-        img = skimage.io.imread(img_path)
-        img_set[i]=skimage.transform.resize(img,reshape_size)
-        for j in range(14):
-                if active_joint_absence and joints_set[i][j][0]==-1: joints_set[i][j][0]=-1
-                else: joints_set[i][j][0]=joints_set[i][j][0]*(reshape_size[0]/img.shape[1])
-                if active_joint_absence and joints_set[i][j][1]==-1: joints_set[i][j][1]=-1
-                else: joints_set[i][j][1]=joints_set[i][j][1]*(reshape_size[1]/img.shape[0])
-        i+=1
-    if divide_train_test:
-        return {'images':img_set[:1800], 'joints':joints_set[:1800]}, {'images':img_set[1800:], 'joints':joints_set[1800:]} 
-    elif not divide_train_test:
-        return {'images':img_set, 'joints':joints_set}
+    def __create(DATASET_ROOT="./dataset/", reshape_size=[400,400],active_joint_absence=False, divide_train_test=True):
+        joints = loadmat(DATASET_ROOT+"joints.mat")
 
-def feed_batch(dataset, batch_size, num):
-    return copy.copy(dataset["images"][num*batch_size:(num+1)*batch_size]), copy.copy(dataset["joints"][num*batch_size:(num+1)*batch_size])
+        if active_joint_absence :
+            joints = joints["joints"].transpose(2,1,0)[:, :, :]
+            for i in range(joints.shape[0]):
+                for j in range(joints.shape[1]):
+                    if joints[i][j][2] == 1: joints[i][j][:1]=[-1, -1]
+            joints_set = joints[:,:,:2]
+        else :
+            joints_set = joints["joints"].transpose(2,1,0)[:, :, :2]
+
+        i=0
+        img_set = np.zeros([len(joints_set),reshape_size[0],reshape_size[1],3])
+        for img_path in sorted(glob.glob(os.path.join(DATASET_ROOT+"images/", '*.jpg'))):
+            img = skimage.io.imread(img_path)
+            img_set[i]=skimage.transform.resize(img,reshape_size)
+            for j in range(14):
+                    if active_joint_absence and joints_set[i][j][0]==-1: joints_set[i][j][0]=-1
+                    else: joints_set[i][j][0]=joints_set[i][j][0]*(reshape_size[0]/img.shape[1])
+                    if active_joint_absence and joints_set[i][j][1]==-1: joints_set[i][j][1]=-1
+                    else: joints_set[i][j][1]=joints_set[i][j][1]*(reshape_size[1]/img.shape[0])
+            i+=1
+        if divide_train_test:
+            return {'images':img_set[:1800], 'joints':joints_set[:1800]}, {'images':img_set[1800:], 'joints':joints_set[1800:]} 
+        elif not divide_train_test:
+            return {'images':img_set, 'joints':joints_set}
+
+    def feed_batch(self, batch_size, num):
+        return copy.copy(self["images"][num*batch_size:(num+1)*batch_size]), copy.copy(self["joints"][num*batch_size:(num+1)*batch_size])
+
