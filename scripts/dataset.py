5a417a29cb9f9602c539acf9d4ccd1ffd462e5fb
Hahnnz <lphion96@naver.com>
2018-04-30 19:19:25 +0900

    modified and added process bar

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import skimage, scipy, glob, os, copy
 from skimage import io, transform
 from scipy.io import loadmat
+from tqdm import tqdm
 
 class lsp:
     def __init__(self):
@@ -39,7 +40,7 @@ class lsp:
         return copy.copy(self["images"][num*batch_size:(num+1)*batch_size]), copy.copy(self["joints"][num*batch_size:(num+1)*batch_size])
 
 class met:
-    def __init__(self, csv_file, re_img_size=(227,227),Rotate=False, Fliplr=False, Shuffle=False):
+    def __init__(self, csv_file, re_img_size=(227,227), is_valid=False, Rotate=False, Fliplr=False, Shuffle=False):
         joints=pd.read_csv(csv_file,header=None).as_matrix()
         
         self.img_path=list(path for path in joints[:,0])
@@ -49,19 +50,21 @@ class met:
         self.img_set=np.zeros([len(self.img_path),re_img_size[0],re_img_size[1],3],dtype=np.float)
         self.coor_set=np.array(self.joint_coors).reshape(len(self.joint_coors),14,2)
         
-        for i, path in enumerate(self.img_path):
-            img=skimage.io.imread(path)
-            self.img_set[i]=skimage.transform.resize(img,(re_img_size[0],re_img_size[1],3))
+        with tqdm(total=len(self.img_path)) as pbar_process:
+            pbar_process.set_description("[Processing Images & Coordinates]")
+            for i, path in enumerate(self.img_path):
+                img=skimage.io.imread(path)
+                self.img_set[i]=skimage.transform.resize(img,(re_img_size[0],re_img_size[1],3))
 
-            for j in range(len(self.joint_coors)):
-                if self.is_valid and bool(self.joint_is_valid[i][j]): self.coor_set[i][j] = [-1,-1]
+                for j in range(len(self.coor_set[i])):
+                    if is_valid and bool(self.joint_is_valid[i][j]): self.coor_set[i][j] = [-1,-1]
 
-                if self.coor_set[i][j][0]==-1: pass
-                else: self.coor_set[i][j][0]=self.coor_set[i][j][0]*(re_img_size[0]/img.shape[1])
-
-                if self.coor_set[i][j][1]==-1: pass
-                else: self.coor_set[i][j][1]=self.coor_set[i][j][1]*(re_img_size[1]/img.shape[0])
-        
+                    if self.coor_set[i][j][0] == -1: pass
+                    else:
+                            self.coor_set[i][j][0] = self.coor_set[i][j][0]*(re_img_size[0]/img.shape[1])
+                            self.coor_set[i][j][1]=self.coor_set[i][j][1]*(re_img_size[1]/img.shape[0])
+                pbar_process.update(1)
+ 
         if Rotate :
             rotated = _rotation(copy.copy(self.img_set), copy.copy(self.coor_set))
             self.img_set = np.concatenate((self.img_set, rotated['images']), axis=0)
@@ -86,21 +89,24 @@ class met:
         rotated_coor = np.zeros([joints.shape[0]*len(thetas), joints.shape[1],2])
         rotated_valid = joint_is_valid
         
-        for i, img in enumerate(images):
-            for j, theta in enumerate(thetas):
-                img_rotated = scipy.ndimage.rotate(img, theta)
-                org_center = (np.array(img.shape[:2][::-1])-1)/2
-                rotated_center = (np.array(img_rotated.shape[:2][::-1])-1)/2
-                
-                coor_list = list(np.array(coor)+org_center for coor in joints[i])
-                rotated_coor_list = list((lambda x : (x[0]*np.cos(theta) + x[1]*np.sin(theta) + rotated_center[0],
-                                                      -x[0]*np.sin(theta) + x[1]*np.cos(theta) + rotated_center[1])
-                                         )(coor) for coor in coor_list)
-                
-                img_rotated = skimage.transform.resize(img_rotated, (re_img_size[0],re_img_size[1],3))
-                
-                rotated_img[(i*len(thetas))+j]=img_rotated
-                rotated_coor[(i*len(thetas))+j]=np.array(rotated_coor_list)*(re_img_size[0]/img_rotated.shape[0])
+        with tqdm(total=len(images)*len(thetas)) as pbar:
+            pbar.set_description("[Rotating Images & Coordinates]")
+            for i, img in enumerate(images):
+                for j, theta in enumerate(thetas):
+                    img_rotated = scipy.ndimage.rotate(img, theta)
+                    org_center = (np.array(img.shape[:2][::-1])-1)/2
+                    rotated_center = (np.array(img_rotated.shape[:2][::-1])-1)/2
+
+                    coor_list = list(np.array(coor)+org_center for coor in joints[i])
+                    rotated_coor_list = list((lambda x : (x[0]*np.cos(theta) + x[1]*np.sin(theta) + rotated_center[0],
+                                                          -x[0]*np.sin(theta) + x[1]*np.cos(theta) + rotated_center[1])
+                                             )(coor) for coor in coor_list)
+
+                    img_rotated = skimage.transform.resize(img_rotated, (re_img_size[0],re_img_size[1],3))
+
+                    rotated_img[(i*len(thetas))+j]=img_rotated
+                    rotated_coor[(i*len(thetas))+j]=np.array(rotated_coor_list)*(re_img_size[0]/img_rotated.shape[0])
+                    pbar.update(1)
         
         for i in range(len(thetas)-1):
             rotated_valid = np.concatenate((rotated_valid, joint_is_valid), axis=0)
@@ -111,15 +117,17 @@ class met:
         mirrored_img = np.zeros([images.shape[0], images.shape[1],images.shape[2],3])
         mirrored_coor = np.zeros([joints.shape[0], joints.shape[1],2])
         
-        for i, img in enumerate(images):
-            mirrored_img[i] = np.fliplr(img)
-            for j, joint in enumerate(joints[i]):
-                mirrored_coor[i][j][1] = joint[1]
-                if joint[0] > (img.shape[0]/2):
-                    mirrored_coor[i][j][0] = (lambda x : x-2*(x-(img.shape[0]/2)))(joint[0])
-                else:
-                    mirrored_coor[i][j][0] = (lambda x : x+2*((img.shape[0]/2)-x))(joint[0])
-        
+        with tqdm(total=len(images)) as pbar:
+            pbar.set_description("[Mirroring Images & Coordinates]")
+            for i, img in enumerate(images):
+                mirrored_img[i] = np.fliplr(img)
+                for j, joint in enumerate(joints[i]):
+                    mirrored_coor[i][j][1] = joint[1]
+                    if joint[0] > (img.shape[0]/2):
+                        mirrored_coor[i][j][0] = (lambda x : x-2*(x-(img.shape[0]/2)))(joint[0])
+                    else:
+                        mirrored_coor[i][j][0] = (lambda x : x+2*((img.shape[0]/2)-x))(joint[0])
+                pbar.update(1)
         return {'images':mirrored_img,'joints':mirrored_coor,'valid':joint_is_valid}
     
     def _shuffling(images, joints, joint_is_valid):
