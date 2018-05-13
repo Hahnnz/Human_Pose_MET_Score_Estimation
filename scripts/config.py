a25b28d27f6a258575c6fc56df7d29c18034401a
Hahnnz <lphion96@naver.com>
2018-05-14 00:32:39 +0900

    added some

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,15 @@
 import os
 
-ROOT_DIR = os.path.expanduser('~/Human_Movement_Estimation')
-OUTPUT_DIR = os.path.join(ROOT_DIR, 'out')
+ROOT_DIR = os.path.expanduser(%pwd+'/')
+OUTPUT_DIR = os.path.join(ROOT_DIR, 'out/')
 
-MET_DATASET_ROOT = os.path.joint(ROOT_DIR, 'dataset')
+MET_DATASET_ROOT = os.path.join(ROOT_DIR, 'dataset/')
+
+filewriter_path = ROOT_DIR + "out/tensorboard/"
+checkpoint_path = ROOT_DIR + "out/checkpoints/"
+
+if not os.path.isdir(filewriter_path):
+    os.mkdir(filewriter_path)
+
+if not os.path.isdir(checkpoint_path):
+    os.mkdir(checkpoint_path)
