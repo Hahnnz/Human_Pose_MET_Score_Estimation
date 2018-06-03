9d547d8665880338f4c07602f0bc5dabde871f28
Hahnnz <lphion96@naver.com>
2018-06-04 01:51:20 +0900

    delete unneeded func

--- models/regressionnet.py
+++ models/regressionnet.py
@@ -7,25 +7,38 @@ import numpy as np
 import copy, math
 from tqdm import tqdm
 
-def create_regression_net(batch_size, data_shape ,num_joints, optimizer_type=None ,net_type="alexnet"):
+def create_regression_net(batch_size, data_shape,
+                          num_joints, 
+                          optimizer_type=None,
+                          net_type="alexnet"):
+    
+    # Create Regressionnet. - Alexnet, Resnet
     with tf.Graph().as_default():
+
         if net_type == "alexnet":
-            net = alexnet.alexnet(batch_size=100,input_shape=(data_shape),output_shape=(num_joints*2,))
+            net = alexnet.alexnet(batch_size=batch_size,
+                                  input_shape=(data_shape),output_shape=(num_joints*2,))
             drop7 = net.get_layers("drop7")
-            net.fc_regression = fc(drop7, int(drop7.get_shape()[1]), num_joints*2, name="fc_regression", relu=False)
-        elif net_type == "resnet": raise ValueError("regressionnet for ResNet will be updated soon")
-        else : raise ValueError("net type should be 'alexnet'. resnet will be updated soon")
+            net.fc_regression = fc(drop7, int(drop7.get_shape()[1]),
+                                   num_joints*2, name="fc_regression", relu=False)
+        elif net_type == "resnet": 
+            raise ValueError("regressionnet for ResNet will be updated soon")
+        else : 
+            raise ValueError("net type should be 'alexnet'. resnet will be updated soon")
+            
         with tf.name_scope("PoseInput"):
-            joints_gt = tf.placeholder(tf.float32, [None, num_joints, 2], name="joints_ground_truth")
-            joints_is_valid = tf.placeholder(tf.int32, [None, num_joints, 2], name="joints_is_valid")
+            joints_gt = tf.placeholder(tf.float32, [batch_size, num_joints*2],
+                                       name="joints_ground_truth")
+            joints_is_valid = tf.placeholder(tf.float32, [batch_size, num_joints*2], 
+                                             name="joints_is_valid")
 
-        joints_gt_flatted = tf.reshape(joints_gt, [-1, num_joints*2])
-        joints_is_valid_flatted = tf.cast(tf.reshape(joints_is_valid, shape=[-1, num_joints*2]), tf.float32)
+        #joints_gt_flatted = tf.reshape(joints_gt, [-1, num_joints*2])
+        #joints_is_valid_flatted = tf.cast(tf.reshape(joints_is_valid, shape=[-1, num_joints*2]), tf.float32)
+        
+        diff = tf.subtract(joints_gt, net.fc_regression)
+        diff_valid = tf.multiply(diff, joints_is_valid)
 
-        diff = tf.subtract(joints_gt_flatted, net.fc_regression)
-        diff_valid = tf.multiply(diff, joints_is_valid_flatted)
-
-        num_valid_joints = tf.reduce_sum(joints_is_valid_flatted, axis=1) / tf.constant(2.0, dtype=tf.float32)
+        num_valid_joints = tf.reduce_sum(joints_is_valid, axis=1) / tf.constant(2.0, dtype=tf.float32)
 
         pose_loss_op = tf.reduce_mean(tf.reduce_sum(tf.pow(diff_valid, 2), axis=1) / num_valid_joints, name="joint_euclidean_loss")
         l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
@@ -48,28 +61,6 @@ def create_regression_net(batch_size, data_shape ,num_joints, optimizer_type=Non
         net.sess.run(tf.variables_initializer(uninit_vars))
     
     return net, loss_with_decay_op, pose_loss_op, train_op
-
-def batch2feeds(batch):
-    images, joints_gt, joints_is_valid = zip(copy.copy(batch))
-    return images, joints_gt, joints_is_valid
-
-def fill_joint_feed_dict(net, batch_feeds, conv_lr=None, fc_lr=None, phase="test", train_keep_prob=0.4):
-    if phase=="train":
-        keep_prob=train_keep_prob
-        is_phase_train = True
-    elif phase=="test":
-        keep_prob=1.0
-        is_phase_train = False
-    else: raise ValueError("phase must be 'train' or 'test'.")
-    
-    if len(batch_size)!=3:
-        raise ValueError("feeds must cotain only 3 elements : images, joints_gt, joints_is_valid")
-    return {net.x: images,
-            "pose_input/joints_gt:0": joints_gt,
-            "pose_input/joints_is_valid:0": joints_is_valid,
-            "input/is_phase_train:0": is_phase_train,
-            "lr/conv_lr:0": conv_lr,
-            "lr/fc_lr:0": fc_lr }
 
 def get_metric(joints_gt, predicted_joints, orig_bboxes, metric_name="PCP"):
     joint_gt=joints_gt.copy()
@@ -98,14 +89,26 @@ def create_summary(tag, value):
     
 def evaluate_pcp(net, pose_loss_op, test_iterator, summary_writer):
     test_iter = copy.copy(test_iterator)
-    num_test_examples = len(test_iter.dataset)
+    num_test_examples = test_iter.img_set.get_shape().as_list()[0]
     num_batches = int(math.ceil(num_test_examples/test_iter.batch_size))
+    next_batch = test_iterator.iterator.get_next()
     num_joints = int(int(net.fc_regression.get_shape()[1])/2)
     
-    joints_gt = joints_is_valid = predicted_joints = orig_bboxes = list()
+    joints_gt = list()
+    joints_is_valid = list()
+    predicted_joints = list()
+    orig_bboxes = list()
     
-    total_loss=0.0
+    total_loss=0.
     
+    with tqdm(total=num_batches) as pbar:
+        for step in range(num_batches):
+            img_batch, joints_batch = net.sess.run(test_iter.next_batch)
+            feed_dict = {net.x : img_batch, net.y_gt: joints_batch, keep_prob: 1.0}
+            pred_joint, batch_loss = net.sess.run([net.fc_regression, pose_loss_op], feed_dict=feed_dict)
+            
+            
+    """
     for i, batch in tqdm(enumerate(test_iter), total=num_batches):
         feeds = batch2feeds(batch)
         feed_dict = fill_joint_feed_dict(net, feeds, conv_lr=0.0, fc_lr=0.0)
@@ -115,7 +118,7 @@ def evaluate_pcp(net, pose_loss_op, test_iterator, summary_writer):
         joints_is_valid.append(feeds[2])
         predicted_joints.append(pred_j.reshape(-1, num_joints, 2))
         orig_bboxes.append(np.vstack([x["bbox"] for x in feeds[3]]))
-    
+    """
     avg_loss = total_loss/num_test_examples
     joints_gt=np.vstack(joints_gt)
     joints_is_valid=np.vstack(joints_is_valid)
