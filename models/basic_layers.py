790157a72b8cc2cff1c0e99dc0669bd65e29eae6
Hahnnz <lphion96@naver.com>
2018-06-04 02:16:45 +0900

    clear

--- models/basic_layers.py
+++ models/basic_layers.py
@@ -2,10 +2,25 @@ import tensorflow as tf
 
 def conv(data, ksize, filters, ssize, padding, use_bias, conv_name=None, bn_name=None, bn=False, act=True):
     if not bn :
-        if act : output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters, strides=(ssize,ssize), padding=padding, name=conv_name, activation=tf.nn.relu,use_bias=use_bias, kernel_initializer=tf.contrib.layers.xavier_initializer())
-        else : output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters, strides=(ssize,ssize), padding=padding, name=conv_name,use_bias=use_bias, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        if act : 
+            output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
+                                      strides=(ssize,ssize),
+                                      padding=padding,
+                                      name=conv_name, 
+                                      activation=tf.nn.relu,use_bias=use_bias,
+                                      kernel_initializer=tf.contrib.layers.xavier_initializer())
+        else : 
+            output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
+                                      strides=(ssize,ssize),
+                                      padding=padding,
+                                      name=conv_name,use_bias=use_bias,
+                                      kernel_initializer=tf.contrib.layers.xavier_initializer())
     else : 
-        conv = tf.layers.conv2d(data, kernel_size=ksize, filters=filters, strides=(ssize,ssize), padding=padding, name=conv_name,use_bias=use_bias, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        conv = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
+                                strides=(ssize,ssize),
+                                padding=padding,
+                                name=conv_name,use_bias=use_bias,
+                                kernel_initializer=tf.contrib.layers.xavier_initializer())
         with tf.variable_scope(bn_name) as bn_name:
             output = tf.contrib.layers.batch_norm(conv)
         if act : output = tf.nn.relu(output)
@@ -30,8 +45,10 @@ def fc(data, num_in, num_out, name=None, relu=True):
         weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
         biases = tf.get_variable('biases', [num_out], trainable=True)
         output = tf.nn.xw_plus_b(data, weights, biases, name=scope.name)
-    if relu : return tf.nn.relu(output)
-    else: return output
+    if relu : 
+        return tf.nn.relu(output)
+    else: 
+        return output
 
 def ZeroPadding2D(data,psize,Type="CONSTANT",name=None):
     data_shape=data.get_shape().as_list()
@@ -48,6 +65,8 @@ def ZeroPadding2D(data,psize,Type="CONSTANT",name=None):
                 padded_2 = tf.expand_dims(tf.expand_dims(padded_2,axis=0),axis=3)
                 padded = tf.concat([padded,padded_2],3)
 
-            if batch_size==0 : padded_dataset=padded
-            else : padded_dataset=tf.concat([padded_dataset,padded],0)
+            if batch_size==0 : 
+                padded_dataset=padded
+            else : 
+                padded_dataset=tf.concat([padded_dataset,padded],0)
     return padded_dataset
