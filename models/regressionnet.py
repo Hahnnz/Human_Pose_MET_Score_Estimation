bbea49eb9cb00eff464c94676d2527d05c2012b2
Hahnnz <lphion96@naver.com>
2018-07-15 10:25:47 +0900

    moved 'set_op'

--- models/regressionnet.py
+++ models/regressionnet.py
@@ -1,4 +1,6 @@
 from models import alexnet
+from models import Convnet1
+from models import Convnet2
 from models.layers import *
 from scripts import tools
 import tensorflow as tf
@@ -10,15 +12,31 @@ from tqdm import tqdm
 def create_regression_net(data_shape,
                           num_joints,
                           batch_size = None,
+                          gpu_memory_fraction=None,
                           optimizer_type="adam",
                           net_type="alexnet"):
     
-    # Create Regressionnet. - Alexnet, Resnet
+    # Create Regressionnet. - Alexnet, Resnet, Convnet1
     with tf.Graph().as_default():
 
         if net_type == "alexnet":
             net = alexnet.alexnet(batch_size=batch_size,
-                                  input_shape=(data_shape),output_shape=(num_joints*2,))
+                                  input_shape=(data_shape),output_shape=(num_joints*2,),
+                                 gpu_memory_fraction=gpu_memory_fraction)
+            drop7 = net.get_layers("drop7")
+            net.fc_regression = fc(drop7, int(drop7.get_shape()[1]),
+                                   num_joints*2, name="fc_regression", relu=False)
+        elif net_type == "convnet1":
+            net = Convnet1.convNet(batch_size=batch_size,
+                                  input_shape=(data_shape),output_shape=(num_joints*2,),
+                                 gpu_memory_fraction=gpu_memory_fraction)
+            drop7 = net.get_layers("drop7")
+            net.fc_regression = fc(drop7, int(drop7.get_shape()[1]),
+                                   num_joints*2, name="fc_regression", relu=False)
+        elif net_type == "convnet2":
+            net = Convnet2.convNet(batch_size=batch_size,
+                                  input_shape=(data_shape),output_shape=(num_joints*2,),
+                                 gpu_memory_fraction=gpu_memory_fraction)
             drop7 = net.get_layers("drop7")
             net.fc_regression = fc(drop7, int(drop7.get_shape()[1]),
                                    num_joints*2, name="fc_regression", relu=False)
@@ -52,7 +70,7 @@ def create_regression_net(data_shape,
         tf.summary.scalar("fc_lr", fc_lr_pl)
             
         net.sess.run(tf.global_variables_initializer())
-        train_op = tools.tftools.set_op(net, pose_loss_op, fc_lr=fc_lr_pl, conv_lr=conv_lr_pl, optimizer_type=optimizer_type)
+        train_op = set_op(net, pose_loss_op, fc_lr=fc_lr_pl, conv_lr=conv_lr_pl, optimizer_type=optimizer_type)
 
         uninit_vars = [v for v in tf.global_variables()
                       if not tf.is_variable_initialized(v).eval(session=net.sess)]
@@ -126,3 +144,42 @@ def evaluate_pcp(net, pose_loss_op, test_iterator, summary_writer):
     pcp_per_stick = get_metric(joints_gt, predicted_joints, orig_bboxes)
     
     return pcp_per_stick
+
+
+def set_op(net, loss_op, fc_lr, conv_lr, optimizer_type="adam"):
+    with net.graph.as_default():
+        if optimizer_type=="adam":
+            conv_optimizer = tf.train.AdamOptimizer(conv_lr)
+            fc_optimizer = tf.train.AdamOptimizer(fc_lr)
+        elif optimizer_type == "adagrad":
+            conv_optimizer = tf.train.AdagradOptimizer(conv_lr, initial_accumulator_value=0.0001)
+            fc_optimizer = tf.train.AdagradOptimizer(fc_lr, initial_accumulator_value=0.0001)
+        elif optimizer_type == "sgd":
+            conv_optimizer = tf.train.GradientDescentOptimizer(conv_lr)
+            fc_optimizer = tf.train.GradientDescentOptimizer(fc_lr)
+        elif optimizer_type == "momentum":
+            conv_optimizer = tf.train.MomentumOptimizer(conv_lr, momentum=0.9)
+            fc_optimizer = tf.train.MomentumOptimizer(fc_lr, momentum=0.9)
+        elif optimizer_type == "adadelta":
+            conv_optimizer = tf.train.AdadeltaOptimizer(learning_rate=conv_lr,rho=0.95,epsilon=1e-09)
+            fc_optimizer = tf.train.AdadeltaOptimizer(learning_rate=conv_lr,rho=0.95,epsilon=1e-09)
+        else : raise ValueError("{} optimizer doesn't exist.".format(optimizer_type))
+
+        conv_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "conv")
+        fc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "fc")
+
+        grads = tf.gradients(loss_op, conv_vars + fc_vars)
+        conv_grads = grads[:len(conv_vars)]
+        fc_grads = grads[len(conv_vars):]
+
+        with tf.name_scope("grad_norms"):
+            for v, grad in zip(conv_vars + fc_vars, grads):
+                if grad is not None :
+                    grad_norm_op = tf.nn.l2_loss(grad, name=format(v.name[:-2]))
+                    tf.add_to_collection("grads", grad_norm_op)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            conv_train_op = conv_optimizer.apply_gradients(zip(conv_grads, conv_vars), name="conv_train_op")
+        fc_train_op = fc_optimizer.apply_gradients(zip(fc_grads, fc_vars), global_step=net.global_iter_counter, name="fc_train_op")
+
+    return tf.group(conv_train_op, fc_train_op)
