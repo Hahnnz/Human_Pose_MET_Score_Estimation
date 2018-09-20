946a2aeaa3fb3aead1b9d19944c821d78ebecdb0
hahnnz <lphion96@naver.com>
2018-09-21 00:41:18 +0900

    added Deconvolutions

--- models/layers.py
+++ models/layers.py
@@ -26,6 +26,32 @@ def conv(data, ksize, filters, ssize, padding, use_bias, conv_name=None, bn_name
         if act : output = tf.nn.relu(output)
     return output
 
+def deconv(data, ksize, filters, ssize, padding, use_bias, deconv_name=None, bn_name=None, bn=False, act=True):
+    if not bn :
+        if act : 
+            output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
+                                                strides=(ssize,ssize),
+                                                padding=padding,
+                                                name=deconv_name, 
+                                                activation=tf.nn.relu,use_bias=use_bias,
+                                                kernel_initializer=tf.contrib.layers.xavier_initializer())
+        else : 
+            output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
+                                                strides=(ssize,ssize),
+                                                padding=padding,
+                                                name=deconv_name,use_bias=use_bias,
+                                                kernel_initializer=tf.contrib.layers.xavier_initializer())
+    else : 
+        deconv = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
+                                          strides=(ssize,ssize),
+                                          padding=padding,
+                                          name=deconv_name,use_bias=use_bias,
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer())
+        with tf.variable_scope(bn_name) as bn_name:
+            output = tf.contrib.layers.batch_norm(deconv)
+        if act : output = tf.nn.relu(output)
+    return output
+
 def max_pooling(data, ksize=3, ssize=2, name=None):
     return tf.nn.max_pool(data, ksize=[1,ksize,ksize,1], strides=[1,ssize,ssize,1], padding="SAME", name=name)
 
