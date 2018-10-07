a550af2fafad72fb7d2811c5a51a51a2316a74ba
Hahnnz <lphion96@naver.com>
2018-10-07 11:50:01 +0900

    updated layers

--- models/layers.py
+++ models/layers.py
@@ -1,55 +1,31 @@
 import tensorflow as tf
 
-def conv(data, ksize, filters, ssize, padding, use_bias, conv_name=None, bn_name=None, bn=False, act=True):
-    if not bn :
-        if act : 
-            output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
-                                      strides=(ssize,ssize),
-                                      padding=padding,
-                                      name=conv_name, 
-                                      activation=tf.nn.relu,use_bias=use_bias,
-                                      kernel_initializer=tf.contrib.layers.xavier_initializer())
-        else : 
-            output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
-                                      strides=(ssize,ssize),
-                                      padding=padding,
-                                      name=conv_name,use_bias=use_bias,
-                                      kernel_initializer=tf.contrib.layers.xavier_initializer())
-    else : 
-        conv = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
-                                strides=(ssize,ssize),
-                                padding=padding,
-                                name=conv_name,use_bias=use_bias,
-                                kernel_initializer=tf.contrib.layers.xavier_initializer())
-        with tf.variable_scope(bn_name) as bn_name:
-            output = tf.contrib.layers.batch_norm(conv)
-        if act : output = tf.nn.relu(output)
+def conv(data, ksize, filters, ssize, padding, use_bias, conv_name=None, bn_name=None, bn=False, act=True, is_train=None):
+    if bn and is_train==None:
+        raise ValueError('Phase should be declared "True" for train or "False" for test when you activate batch normalization.')
+    
+    output = tf.layers.conv2d(data, kernel_size=ksize, filters=filters,
+                              strides=(ssize,ssize),
+                              padding=padding.upper(),
+                              name=conv_name,use_bias=use_bias,
+                              kernel_initializer=tf.contrib.layers.xavier_initializer())
+    
+    if bn : output = batch_norm(output, is_train, bn_name)
+    if act : output = tf.nn.relu(output)
     return output
 
-def deconv(data, ksize, filters, ssize, padding, use_bias, deconv_name=None, bn_name=None, bn=False, act=True):
-    if not bn :
-        if act : 
-            output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
-                                                strides=(ssize,ssize),
-                                                padding=padding,
-                                                name=deconv_name, 
-                                                activation=tf.nn.relu,use_bias=use_bias,
-                                                kernel_initializer=tf.contrib.layers.xavier_initializer())
-        else : 
-            output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
-                                                strides=(ssize,ssize),
-                                                padding=padding,
-                                                name=deconv_name,use_bias=use_bias,
-                                                kernel_initializer=tf.contrib.layers.xavier_initializer())
-    else : 
-        deconv = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
-                                          strides=(ssize,ssize),
-                                          padding=padding,
-                                          name=deconv_name,use_bias=use_bias,
-                                          kernel_initializer=tf.contrib.layers.xavier_initializer())
-        with tf.variable_scope(bn_name) as bn_name:
-            output = tf.contrib.layers.batch_norm(deconv)
-        if act : output = tf.nn.relu(output)
+def deconv(data, ksize, filters, ssize, padding, use_bias, deconv_name=None, bn_name=None, bn=False, act=True, is_train=None):
+    if bn and is_train==None:
+        raise ValueError('Phase should be declared "True" for train or "False" for test when you activate batch normalization.')
+    
+    output = tf.layers.conv2d_transpose(data, kernel_size=ksize, filters=filters,
+                                        strides=(ssize,ssize),
+                                        padding=padding.upper(),
+                                        name=deconv_name, use_bias=use_bias,
+                                        kernel_initializer=tf.contrib.layers.xavier_initializer())
+    
+    if bn : output = batch_norm(output, is_train, bn_name)
+    if act : output = tf.nn.relu(output)
     return output
 
 def max_pooling(data, ksize=3, ssize=2, name=None):
@@ -61,41 +37,18 @@ def dropout(data, ratio, name=None):
 def lrn(data, depth_radius, alpha, beta, name=None):
     return tf.nn.local_response_normalization(data, depth_radius=depth_radius, alpha=alpha, beta=beta, bias=1.0, name=name)
 
-def bn(data, name=None):
-    with tf.variable_scope(name) as name:
-        batch_norm = tf.contrib.layers.batch_norm(data)
-    return batch_norm
+def batch_norm(data, is_train, name=None):
+    return tf.layers.batch_normalization(data, training=is_train, name=name)
 
-def fc(data, num_in, num_out, name=None, relu=True, bn=True):
+def fc(data, num_in, num_out, name=None, relu=True, bn=True, is_train=None):
+    if bn and is_train==None:
+        raise ValueError('Phase should be declared "True" for train or "False" for test when you activate batch normalization.')
+        
     with tf.variable_scope(name) as scope:
         weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
         biases = tf.get_variable('biases', [num_out], trainable=True)
         output = tf.nn.xw_plus_b(data, weights, biases, name=scope.name)
-        
-    if relu : 
-        output = tf.nn.relu(output)
-    if bn :
-        output = bn(output)
+    if bn : output = batch_norm(output, is_train)
+    if relu : output = tf.nn.relu(output)
         
     return output
-
-def ZeroPadding2D(data,psize,Type="CONSTANT",name=None):
-    data_shape=data.get_shape().as_list()
-    if data_shape[0]==None :
-        raise ValueError("batch_size must be specified.")
-    else:
-        paddings = tf.constant([psize, psize])
-        for batch_size in range(data_shape[0]):
-            padded = tf.pad(data[0,:,:,0],paddings,Type)
-            padded = tf.expand_dims(tf.expand_dims(padded,axis=0),axis=3)
-
-            for channels in range(1,data_shape[3]):
-                padded_2 = tf.pad(data[0,:,:,channels],paddings,Type)
-                padded_2 = tf.expand_dims(tf.expand_dims(padded_2,axis=0),axis=3)
-                padded = tf.concat([padded,padded_2],3)
-
-            if batch_size==0 : 
-                padded_dataset=padded
-            else : 
-                padded_dataset=tf.concat([padded_dataset,padded],0)
-    return padded_dataset
