c904499c85a86dfb8b0a876b9742329c157f2d42
hahnnz <lphion96@naver.com>
2018-12-28 02:26:33 +0900

    removed paths

--- scripts/config.py
+++ scripts/config.py
@@ -3,20 +3,6 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-ROOT_DIR = os.path.expanduser(os.getcwd()+'/')
-OUTPUT_DIR = os.path.join(ROOT_DIR, 'out/')
-
-MET_DATASET_ROOT = os.path.join(ROOT_DIR, 'dataset/')
-
-filewriter_path = ROOT_DIR + "out/tensorboard/"
-checkpoint_path = ROOT_DIR + "out/checkpoints/"
-
-if not os.path.isdir(filewriter_path):
-    os.mkdir(filewriter_path)
-
-if not os.path.isdir(checkpoint_path):
-    os.mkdir(checkpoint_path)
-
 joints = ['Right Ankle', 'Right Knee', 'Right Hip', 'Left Hip', 'Left Knee', 'Left Ankle',
           'Right Wrist', 'Right Elbow', 'Right Shoulder', 'Left Shoulder', 'Left Elbow', 'Left Wrist',
           'Neck', 'Head']
