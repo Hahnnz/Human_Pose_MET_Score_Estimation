a20bd6551cd0293d13a3529a970c850f6fbc23c9
hahnnz <lphion96@naver.com>
2018-12-28 01:08:38 +0900

    added joints, sticks, activities colors

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 ROOT_DIR = os.path.expanduser(os.getcwd()+'/')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'out/')
@@ -13,3 +16,15 @@ if not os.path.isdir(filewriter_path):
 
 if not os.path.isdir(checkpoint_path):
     os.mkdir(checkpoint_path)
+
+joints = ['Right Ankle', 'Right Knee', 'Right Hip', 'Left Hip', 'Left Knee', 'Left Ankle',
+          'Right Wrist', 'Right Elbow', 'Right Shoulder', 'Left Shoulder', 'Left Elbow', 'Left Wrist',
+          'Neck', 'Head']
+
+classes = ['Sleeping','Reclining','Seated.quiet','Standing.Relaxed','Reading.seated',
+              'Writing', 'Typing', 'Filing.Seated','Filing.Stand','Walking about']
+class_color = ['red','orange','green','blue','purple',
+               'aqua','brown','navy','magenta','lightseagreen']
+
+sticks = ['Head','Torso','U Arm','L Arm','U Leg','L Leg','Mean']
+sticks_color = ['red','green','blue','yellow','pink','orange','black']
