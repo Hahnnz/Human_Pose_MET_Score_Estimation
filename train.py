bcfe88ab6dacc854ed323c52b25b842aa4bd0b13
Hahnnz <lphion96@naver.com>
2019-01-02 16:44:32 +0900

    updated to fit new models

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ tools.etc.set_GPU(gpuNum)
 
 batch_size = 40
 iter_num = 10000
-bbox_scale = [(lambda x : x/10)(x) for x in range(10,25)]
+bbox_scale = [x/10 for x in range(10,25)]
 theta_set = list(range(-30,30,5))
 theta_set.remove(0)
 lowest_loss=None
@@ -20,17 +20,17 @@ highest_pcp=None
 
 # load Network
 
-net = Regressionnet(data_shape=(256,256,3),optimizer_type='adadelta',num_joints=14, gpu_memory_fraction=None)
+net = Regressionnet(data_shape=(128,128,3),optimizer_type='adadelta',num_joints=14, gpu_memory_fraction=None)
     
 with net.graph.as_default():
     saver = tf.train.Saver()
 
-    train_it = dataset.met("./dataset/MET6/met5_allcoor_train.csv", batch_size=batch_size,
-                           Rotate=True, Shuffle=True, re_img_size=(256,256), Fliplr=True,
-                           normalize=True, theta_set=theta_set,dataset_root="./dataset/MET6/")
-    test_it = dataset.met("./dataset/MET6/met5_allcoor_test.csv", batch_size=batch_size,
-                          Rotate=True, Shuffle=True, re_img_size=(256,256), Fliplr=True,
-                          normalize=True, theta_set=theta_set,dataset_root="./dataset/MET6/")
+    train_it = dataset.met("./dataset/MET7/met7_train.csv", batch_size=batch_size,
+                           Rotate=True, Shuffle=True, re_img_size=(128,128), Fliplr=True,
+                           normalize=True, theta_set=theta_set,dataset_root="./dataset/MET7/")
+    test_it = dataset.met("./dataset/MET7/met7_test.csv", batch_size=batch_size,
+                          Rotate=True, Shuffle=True, re_img_size=(128,128), Fliplr=True,
+                          normalize=True, theta_set=theta_set,dataset_root="./dataset/MET7/")
     
     summary_writer = tf.summary.FileWriter("./out/gpu"+gpuNum, net.sess.graph)
     summary_op = tf.summary.merge_all()
@@ -44,7 +44,9 @@ with tf.device("/gpu:"+gpuNum):
             #=============================#
             tr_cost = 0.
             tr_cnt = 0
-            tr_acc = []
+            tr_pcp = 0.
+            tr_diff_dv = 0
+            tr_l2_loss = 0
             
             for scale in bbox_scale:
                 for n in range(train_it.num_batchs):
@@ -58,17 +60,19 @@ with tf.device("/gpu:"+gpuNum):
                         batch_data.append(bbox_img)
                         batch_joints.append(bbox_coord)
                     
-                    summary_str, _, loss_value, predicted_joints = net.sess.run(
-                        [summary_op, net.train_op, net.pose_loss_op, net.fc_regression],
+                    summary_str, _, loss_value, predicted_joints, diff_dv, l2_loss = net.sess.run(
+                        [summary_op, net.train_op, net.pose_loss_op, net.fc_regression, net.diff_dv, net.l2_loss],
                         feed_dict={net.x : np.array(batch_data),
                                    net.y : np.array(batch_joints).reshape(-1,28),
                                    net.valid : train_it.batch_set['valid'][n],
-                                   net.lr: 1e-2,
+                                   net.lr: 1.,
                                    net.keep_prob:0.7,
                                    net.is_train:True,
                                    net.Lambda : 1.})
                     
                     tr_cost+=loss_value
+                    tr_diff_dv+=diff_dv
+                    tr_l2_loss+=l2_loss
                     tr_cnt+=1
 
                     predicted_joints = predicted_joints.reshape(-1,14,2)
@@ -76,9 +80,8 @@ with tf.device("/gpu:"+gpuNum):
                     orig_canonical = tools.pose.convert2canonical(np.array(batch_joints).reshape(-1,14,2))
                     pcp_value=tools.pose.eval_strict_pcp(orig_canonical,pred_canonical)
                     average_pcp = tools.pose.average_pcp_left_right_limbs(pcp_value)[0]
-                    tr_acc.append(average_pcp[-1])
+                    tr_pcp += average_pcp[-1]
             
-            tr_acc = sum(tr_acc)/len(tr_acc)
             summary_writer.add_summary(summary_str, step)
             
             #=============================#
@@ -86,7 +89,9 @@ with tf.device("/gpu:"+gpuNum):
             #=============================#
             te_cost = 0.
             te_cnt = 0
-            te_acc = []
+            te_pcp = 0
+            te_diff_dv = 0
+            te_l2_loss = 0
             
             for scale in bbox_scale:
                 for n in range(test_it.num_batchs):
@@ -100,28 +105,36 @@ with tf.device("/gpu:"+gpuNum):
                         batch_data.append(bbox_img)
                         batch_joints.append(bbox_coord)
 
-                    loss_value, predicted_joints = net.sess.run(
-                        [net.pose_loss_op, net.fc_regression],
+                    loss_value, predicted_joints, diff_dv, l2_loss, valid_acc, valid_loss, act_acc, act_loss  = net.sess.run(
+                        [net.pose_loss_op, net.fc_regression, net.diff_dv, net.l2_loss,
+                         net.valid_accuracy, net.valid_loss_op, net.activity_accuracy, net.activity_loss_op],
                         feed_dict={net.x : np.array(batch_data),
                                    net.y : np.array(batch_joints).reshape(-1,28),
                                    net.valid: test_it.batch_set['valid'][n],
                                    net.keep_prob:1.0,
-                                   net.is_train:False})
+                                   net.is_train:False,
+                                   net.Lambda:1.})
                     
                     te_cost+=loss_value
+                    te_diff_dv+=diff_dv
+                    te_l2_loss+=l2_loss
                     te_cnt+=1
+                    
                     predicted_joints = predicted_joints.reshape(-1,14,2)
                     pred_canonical = tools.pose.convert2canonical(predicted_joints)
                     orig_canonical = tools.pose.convert2canonical(np.array(batch_joints).reshape(-1,14,2))
                     pcp_value=tools.pose.eval_strict_pcp(orig_canonical,pred_canonical)
                     average_pcp = tools.pose.average_pcp_left_right_limbs(pcp_value)[0]
-                    te_acc.append(average_pcp[-1])
-            
-            te_acc = sum(te_acc)/len(te_acc)
+                    te_pcp+=average_pcp[-1]
             
             summ = tf.Summary()
-            summ.value.add(tag='Test arverage PCP mean', simple_value=te_acc)
-            summ.value.add(tag='Train arverage PCP mean', simple_value=tr_acc)
+            summ.value.add(tag='Test arverage PCP mean', simple_value=te_pcp/te_cnt)
+            summ.value.add(tag='Test Cosine_distance', simple_value=te_diff_dv/te_cnt)
+            summ.value.add(tag='Test l2 Loss', simple_value=te_l2_loss/te_cnt)
+            
+            summ.value.add(tag='Train arverage PCP mean', simple_value=tr_pcp/tr_cnt)
+            summ.value.add(tag='Train Cosine_distance', simple_value=tr_diff_dv/tr_cnt)
+            summ.value.add(tag='Train l2 Loss', simple_value=tr_l2_loss/tr_cnt)
             
             summary_writer.add_summary(summ,step)
             
@@ -129,11 +142,12 @@ with tf.device("/gpu:"+gpuNum):
                 if lowest_loss == None or lowest_loss > te_cost/te_cnt :
                     lowest_loss = te_cost/te_cnt
                     saver.save(net.sess, "./out/gpu"+gpuNum+"/Regressionnet_lowest_loss.ckpt")
-                if highest_pcp == None or highest_pcp < te_acc :
-                    highest_pcp = te_acc
+                if highest_pcp == None or highest_pcp < te_pcp :
+                    highest_pcp = te_pcp
                     saver.save(net.sess, "./out/gpu"+gpuNum+"/Regressionnet_highest_pcp.ckpt")
                     
             pbar.update(1)
             pbar.set_description("[ Step : "+str(step+1)+"]")
-            pbar.set_postfix_str(" Train Acc : "+'%.6f' % (tr_acc)+" Train Loss : "+'%.4f' % (tr_cost/tr_cnt)+ \
-                                 " Test Acc : "+'%.6f' % (te_acc)+" Test Loss : "+'%.4f' % (te_cost/te_cnt))
+            pbar.set_postfix_str(" Train Acc : "+'%.6f' % (tr_pcp)+" Train Loss : "+'%.4f' % (tr_cost/tr_cnt)+ \
+                                 " Test Acc : "+'%.6f' % (te_pcp)+" Test Loss : "+'%.4f' % (te_cost/te_cnt))
+
